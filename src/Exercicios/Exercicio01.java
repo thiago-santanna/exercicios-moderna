fe9ec45a390ec8a75afe887dcf6f377fd1091c1e
thiago-santanna <uzmaki_thiago@outlook.com>
2022-10-13 15:11:25 -0300

    primeiro exercicio

---
+++ src/Exercicios/Exercicio01.java
@@ -0,0 +1,17 @@
+package Exercicios;
+
+public class Exercicio01 {
+	
+	public void execute() {
+		System.out.println("");		
+		System.out.println("Eu meu chamo: " + "Thiago Sant'anna");
+		System.out.println("Minha filha se chama: " + "Sarah de Oliveira Sant'anna");
+		System.out.println("Minha Esposa chama: " + "Rafaelle de Oliveira ferreira");
+		System.out.println("======================================================");
+		System.out.println("");
+			
+		System.out.println("Soma de (1777 + 9999) = " + (1777 + 9999));
+		System.out.println("Multiplicar (99 * 73) = " + (99 * 73));
+
+	}
+}
---
+++ src/principal/Application.java
@@ -0,0 +1,11 @@
+package principal;
+
+import Exercicios.Exercicio01;
+
+public class Application {
+	public static void main(String[] args) {
+
+		final Exercicio01 exercicio01 = new Exercicio01();
+		exercicio01.execute();
+	}
+}
