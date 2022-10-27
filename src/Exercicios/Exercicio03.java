a65a76a2832b68b8368a9182cd476bd5d37ccb38
thiago-santanna <uzmaki_thiago@outlook.com>
2022-10-27 09:32:06 -0300

    terceiro exercicio

---
+++ src/Exercicios/Exercicio03.java
@@ -0,0 +1,21 @@
+package Exercicios;
+
+public class Exercicio03 {
+
+	public void execute() {
+		/* Primeira parte */
+		long variavelUm = 10;
+		long variavelDois = 15;
+
+		long moduloVariaveis = variavelUm % variavelDois;
+		System.out.println("O módulo das duas variáveis é: " + moduloVariaveis);
+
+		/* Segunda parte */
+		String result = ((123 * 344) + 2344) >= (8799) != false 
+				? "Moderna " + "Tecnologia" 
+				: "Tá saindo !!!";
+		
+		System.out.println(result);
+	}
+
+}
--- src/principal/Application.java
+++ src/principal/Application.java
@@ -2,14 +2,16 @@ package principal;
 
 import Exercicios.Exercicio01;
 import Exercicios.Exercicio02;
+import Exercicios.Exercicio03;
 
 public class Application {
 	public static void main(String[] args) {
 
-		final Exercicio01 exercicio01 = new Exercicio01();
-		exercicio01.execute();
-		
-		final Exercicio02 exercicio02 = new Exercicio02();
-		exercicio02.execute();
+//		final Exercicio01 exercicio01 = new Exercicio01();
+//		exercicio01.execute();		
+//		final Exercicio02 exercicio02 = new Exercicio02();
+//		exercicio02.execute();
+//		final Exercicio03 exercicio03 = new Exercicio03();
+//		exercicio03.execute();
 	}
 }
